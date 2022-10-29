d5126f2ad97b46e10aa55d374fd0ded8a18279cc
katurivinay <85672552+katurivinay@users.noreply.github.com>
2022-10-29 11:10:23 +0530

    Create bottom_view.cpp

---
+++ bottom_view.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+  public:
+  int data;
+  Node*left;
+  Node*right;
+  Node(int d){
+    data=d;
+    left=NULL;
+    right=NULL;
+  }
+};
+Node* buildtree(){
+  int d;
+  cin>>d;
+  Node*root;
+  if(d==-1){
+    return NULL;
+  }
+  root=new Node(d);
+  root->left=buildtree();
+  root->right=buildtree();
+  return root;
+}
+void bottom_view(Node *root){
+    if(root == NULL)return;
+                     //axis    
+    queue<pair<Node*,int>> q;
+    q.push({root,0});
+     //axis,data;
+    map<int,int> mp;
+    while(!q.empty()){
+        auto curr = q.front();
+        q.pop();
+        Node* curr_node = curr.first;
+        int axis = curr.second;
+        mp[axis] = curr_node->data;
+        if(curr_node->left){
+            q.push({curr_node->left,axis-1});
+        }
+        if(curr_node->right){
+            q.push({curr_node->right,axis+1});
+        }
+    }
+    for(auto it : mp)cout<<it.first<<" "<<it.second<<"\n";
+}
+int main()
+{
+    Node*root=buildtree();
+    bottom_view(root);
+  return 0;
+}
